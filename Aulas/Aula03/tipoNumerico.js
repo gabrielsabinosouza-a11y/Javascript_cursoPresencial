e8f6063e9fd0b5134a9e76404050a843917fa33d
CAFÉ <pciskolargx@gmail.com>
2026-02-19 09:17:38 -0300

    tipos numericos aprendidos

---
+++ Aulas/Aula03/tipoNumerico.js
@@ -0,0 +1,30 @@
+const meuNumero = 3;
+
+const primeiroNumero = 21;
+
+const segundoNumero = 3;
+
+const soma = primeiroNumero + segundoNumero;
+const subtracao = primeiroNumero - segundoNumero;
+const multiplicacao = primeiroNumero * segundoNumero;
+const divisao = primeiroNumero / segundoNumero;
+
+console.log(`the sum is: ${soma}`);
+console.log(`the sum is: ${subtracao}`);
+console.log(`the sum is: ${multiplicacao}`);
+console.log(`the sum is: ${divisao.toFixed(2)}`);
+
+//ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+console.log(numeroPontoFlutuante);
+console.log(`Numero sem zero ${pontoFlutuanteSemZero}`);
+
+
+const novaOperacao = numeroPontoFlutuante * pontoFlutuanteSemZero;
+console.log(novaOperacao);
+
+const texto = "text";
+console.log(texto * primeiroNumero);
