f1c2d21c54e3908d458b3e4394d8cf587c959d42
CAFÉ <pciskolargx@gmail.com>
2026-02-24 07:34:32 -0300

    exercicios resolvidos

---
+++ exercicios/atividade1/index.js
@@ -0,0 +1,16 @@
+console.log("Ola, Mundo!");
+
+nome = "CAFE";
+console.log(nome);
+
+endereco = "Rua laercio nakashima 103";
+telefone  = "(41) 99671-3782";
+
+console.log(`Meu nome é ${nome}, moro na ${endereco} e meu telefone é ${telefone}`);
+
+const num1 = 2;
+const num2 = 4;
+
+console.log(`a soma dos numeros {num1} e {num2} é igual a {num1 + num2}`);
+console.log(`o resto da divisao de {num1} e {num2} e igual a {num1 % num2}`);
+console.log(`A divisao entre {num1} e {num2} e igual a {num1 / num2}`);
