f8d66ecf450ab25e9be95e0dc174eccefb067911
CAFÉ <pciskolargx@gmail.com>
2026-03-12 09:21:49 -0300

    area fianlizad

--- Aulas/Aula04/script.js
+++ Aulas/Aula04/script.js
@@ -2,13 +2,13 @@ var altura = 5;
 let area
 var comprimento = 9;
 area = comprimento * altura;
-const forma = "retangulo";
+const forma = "triangulo";
 console.log(comprimento * altura);
 
 if(forma == 'retangulo'){
     area = altura * comprimento;
 } else {
-    area (area * comprimento ) / 2;
+    area = (area * comprimento ) / 2;
 }
 
 console.log(area)
