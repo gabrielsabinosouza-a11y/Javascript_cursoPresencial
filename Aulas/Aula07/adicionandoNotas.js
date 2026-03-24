3ab298c1b8faec6fe5ef9e339d528dfe1d3e8bc3
CAFÉ <pciskolargx@gmail.com>
2026-03-24 11:31:18 -0300

    aula07 finaizada

---
+++ Aulas/Aula07/adicionandoNotas.js
@@ -0,0 +1,13 @@
+//manipulacao com arrays, inseerir novos dados 
+
+let notas = [10, 6, 8];
+
+
+notas = [];
+
+notas.push(7);
+//metodo que coloca mais 
+//um elemento no array
+const media = (notas[0]+notas[1]+notas[2]/notas.length);
+
+console.log(media)
