9fa0af65146974fc279022ab156d59960dbaac94
CAFÉ <pciskolargx@gmail.com>
2026-03-24 10:49:50 -0300

    aula06 finaizado

---
+++ Aulas/Aula06/funcoes.js
@@ -0,0 +1,17 @@
+// let = "";
+// console.log(x);
+// x = "oi"
+
+
+function imprimeTexto(texto){
+    console.log(texto);
+}
+
+imprimeTexto("ola");
+
+function soma(){ 
+    const resultado = 2 + 2;
+    
+}
+
+soma()
---
+++ Aulas/Aula06/template.js
@@ -0,0 +1,6 @@
+let nome = "Gabrielcio";
+let idade = 2026 - 2009;
+let cidadeNascimento = "curitiba";
+
+const apresentacao = `meu nome é ${nome}, tenho ${idade} anos e nasci em ${cidadeNascimento}`;
+console.log(apresentacao);
